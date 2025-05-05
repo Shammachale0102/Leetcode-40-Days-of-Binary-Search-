84b397178ec800178af247367b7d8df8469abdfc
Sham Machale <Shammachale0102@users.noreply.github.com>
2025-05-05 11:53:09 +0530

    Solve Longest Increasing Subsequence

---
+++ Problems/Challenge-28.java
@@ -0,0 +1,20 @@
+/*Longest Increasing Subsequence:
+
+Given an integer array nums, return the length of the longest strictly increasing subsequence. */
+
+import java.util.*;
+
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        List<Integer> sub = new ArrayList<>();
+
+        for (int num : nums) {
+            int i = Collections.binarySearch(sub, num);
+            if (i < 0) i = -(i + 1); 
+            if (i == sub.size()) sub.add(num);
+            else sub.set(i, num);
+        }
+
+        return sub.size();
+    }
+}
