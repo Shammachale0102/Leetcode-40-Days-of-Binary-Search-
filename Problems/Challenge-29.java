df0a972886b40606b308beae60f090de53bfde97
Sham Machale <Shammachale0102@users.noreply.github.com>
2025-05-06 11:16:52 +0530

    Solve Russian Doll Envelopes

---
+++ Problems/Challenge-29.java
@@ -0,0 +1,41 @@
+/*Russian Doll Envelopes:
+
+You are given a 2D array of integers envelopes where envelopes[i] = [wi, hi] represents the width and the height of an envelope.
+
+One envelope can fit into another if and only if both the width and height of one envelope are greater than the other envelope's width and height.
+
+Return the maximum number of envelopes you can Russian doll (i.e., put one inside the other). */
+
+import java.util.*;
+
+class Solution {
+    public int maxEnvelopes(int[][] envelopes) {
+        Arrays.sort(envelopes, (a, b) -> {
+            if (a[0] == b[0])
+                return b[1] - a[1];  
+            else
+                return a[0] - b[0];  
+        });
+
+        int[] heights = new int[envelopes.length];
+        for (int i = 0; i < envelopes.length; i++) {
+            heights[i] = envelopes[i][1];
+        }
+
+        return lengthOfLIS(heights);
+    }
+
+    private int lengthOfLIS(int[] nums) {
+        List<Integer> dp = new ArrayList<>();
+        for (int num : nums) {
+            int idx = Collections.binarySearch(dp, num);
+            if (idx < 0) idx = -idx - 1;
+            if (idx == dp.size()) {
+                dp.add(num);
+            } else {
+                dp.set(idx, num);
+            }
+        }
+        return dp.size();
+    }
+}
